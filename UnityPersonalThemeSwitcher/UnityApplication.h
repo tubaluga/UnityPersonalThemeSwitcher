cf1351b0d16792b8cc64266bace0fdc824d0e7a9
Sergey A. Bazylev <tubaluga@gmail.com>
2018-09-21 10:29:49 +0300

    First commit

---
+++ UnityPersonalThemeSwitcher/UnityApplication.h
@@ -0,0 +1,17 @@
+//
+//  UnityApplication.h
+//  UnityPersonalThemeSwitcher
+//
+//  Created by Sergey A. Bazylev on 20/09/2018.
+//  Copyright © 2018 Sergey A. Bazylev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnityApplication : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UnityPersonalThemeSwitcher/UnityApplication.m
@@ -0,0 +1,13 @@
+//
+//  UnityApplication.m
+//  UnityPersonalThemeSwitcher
+//
+//  Created by Sergey A. Bazylev on 20/09/2018.
+//  Copyright © 2018 Sergey A. Bazylev. All rights reserved.
+//
+
+#import "UnityApplication.h"
+
+@implementation UnityApplication
+
+@end
